25e706206fb8d7578a08cabd7a40033fba12072e
doanpham98 <meodien.tb@gmail.com>
2020-06-10 17:12:42 +0700

    Deploy

--- src/index.js
+++ src/index.js
@@ -11,13 +11,15 @@ import {composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
 
 const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
-ReactDOM.render(
-  // <React.StrictMode>
-  <Provider store={store}><App /></Provider>
-    ,
-  // </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<Provider store={store}><App /></Provider>, rootElement);
+} else {
+  ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
